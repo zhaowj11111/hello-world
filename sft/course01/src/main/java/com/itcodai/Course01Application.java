5373020f7f27509dfc0bec116b47ece2a898dab8
zhaowj11111 <88027379+zhaowj11111@users.noreply.github.com>
2021-07-28 17:31:35 +0800

    Update Course01Application.java
    啥都

--- sft/course01/src/main/java/com/itcodai/Course01Application.java
+++ sft/course01/src/main/java/com/itcodai/Course01Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Course01Application {
 
     public static void main(String[] args) {
+        //碍事会哦哦
+        
         SpringApplication.run(Course01Application.class, args);
     }
 

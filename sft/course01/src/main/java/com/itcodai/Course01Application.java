5de446ed5a781f97936bece04e03dfdb32434d88
zhaowj11111 <88027379+zhaowj11111@users.noreply.github.com>
2021-07-29 17:09:37 +0800

    Update Course01Application.java
    阿斯蒂芬

--- sft/course01/src/main/java/com/itcodai/Course01Application.java
+++ sft/course01/src/main/java/com/itcodai/Course01Application.java
@@ -7,10 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Course01Application {
 
     public static void main(String[] args) {
-        //123
         SpringApplication.run(Course01Application.class, args);
-        //阿斯顿法国的身份
-        //阿斯顿法国的身份
+        //爱谁谁的分公司答复
     }
 
 }
